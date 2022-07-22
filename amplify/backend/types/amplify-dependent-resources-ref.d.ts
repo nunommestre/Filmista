e46b4d2023acdcb82b3a0eeaa7f3e552e0c9904d
Jose Ramos <jmrp.04@gmail.com>
2022-07-22 15:36:58 -0400

    Our app is connected to Amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "Filmista": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ import LoadingScreen from "./Components/Loading";
 
 // ----- 3. External Libraries ----- //
 import { useState, useEffect } from "react";
-
 import { Navbar, Container } from "react-bootstrap";
+import { Amplify, API, graphqlOperation } from "aws-amplify";
+import { withAuthenticator, Button, Heading } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
-function App() {
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+function App({ signOut, user }) {
   // ----- Properties ----- //
 
   const [loading, setLoading] = useState(true);
@@ -51,4 +56,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
