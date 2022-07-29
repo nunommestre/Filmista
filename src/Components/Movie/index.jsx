541b4b1809aabe06adc52c52678115f4510ed1f3
Nuno Mestre <nuno_mestre@student.uml.edu>
2022-07-29 17:21:44 -0400

    Messy but fine for testing buttons

--- src/Components/Movie/index.jsx
+++ src/Components/Movie/index.jsx
@@ -36,25 +36,6 @@ const MovieDisplay = () => {
   const searchUpdate = (e) => {
     setSearch(e.target.value);
   };
-  const Movie = ({ title, poster_path, overview, vote_average }) => {
-    return (
-      <div className="movie-card">
-        <img src= {IMAGE_API + poster_path} alt={title} />
-        <div className="overview">
-            <h6>{"Title: " + title}</h6>
-            <h6>Rating:  <span>{vote_average}</span></h6>
-            <h6>Your Rating:  <span>N/A</span></h6>
-            <h6>Description:</h6>
-            <p>{overview}</p>
-            <div className="movie-buttons">
-            <Button variant="primary" className="movie-button">Rank Movie</Button>
-            <Button variant="primary" className="movie-button">Add to Playlist</Button>
-            <Button variant="warning" className="movie-button">View More</Button>
-            </div>
-        </div>
-      </div>
-    );
-  };
   return (
     <div className="explore-page">
         <div className="page-header">
@@ -67,10 +48,45 @@ const MovieDisplay = () => {
     <div className="movie-grid">
       {movies.map((movie) => (
         <Movie key={movie.id} {...movie} />
-      ))}
+        ))}
     </div>
     </div>
   );
 };
-
 export default MovieDisplay;
+const Movie = ({ title, poster_path, overview, vote_average }) => {
+  const [rateStatus, setRateStatus] = useState(["movie-rank-hidden"]);
+  const rateMovie = () => {
+    setRateStatus("movie-rank");
+  }
+  const hideRateScreen = () => {
+    setRateStatus("movie-rank-hidden");
+  }
+  return (
+    <div className="movie-card">
+      <img src= {IMAGE_API + poster_path} alt={title} />
+      <div className="overview">
+          <h6>{"Title: " + title}</h6>
+          <h6>Rating:  <span>{vote_average}</span></h6>
+          <h6>Your Rating:  <span>N/A</span></h6>
+          <h6>Description:</h6>
+          <p>{overview}</p>
+          <div className="movie-buttons">
+          <Button variant="primary" className="movie-button" onClick={rateMovie}>Rate Movie</Button>
+          <Button variant="primary" className="movie-button">Add to Playlist</Button>
+          <Button variant="warning" className="movie-button">View More</Button>
+          </div>
+      </div>
+      <div className={rateStatus}>
+      <h6>Rate:</h6>
+        <input className="rating-bar" type="number" placeholder="Rating..."/>
+      <h6>Comment: </h6>
+        <input className="comment-field" type="number" placeholder="Comments..."/>
+        <div className="movie-buttons">
+        <Button variant="warning" className="movie-button" onClick={hideRateScreen}>Rate</Button>
+        <Button variant="danger" className="movie-button" onClick={hideRateScreen}>Back</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
