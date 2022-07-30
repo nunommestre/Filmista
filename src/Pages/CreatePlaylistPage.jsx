eca1caec0230bb41e6e27a971201cec57388fc8e
Nuno Mestre <nuno_mestre@student.uml.edu>
2022-07-30 01:16:36 -0400

    add Edit Account and playlist UI

--- src/Pages/CreatePlaylistPage.jsx
+++ src/Pages/CreatePlaylistPage.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+import "./CSS/createPlaylist.css"
+import{ Button } from "react-bootstrap";
 
 const CreatePlaylistPage = () => {
   // ----- Return Statement ----- //
   return (
+    <div className="create-playlist-page">
     <div>
-      <h1>Create Playlist</h1>
-      <h1>Create Playlist</h1>
-      <h1>Create Playlist</h1>
-      <h1>Create Playlist</h1>
-      <h1>Create Playlist</h1>
-      <h1>Create Playlist</h1>
-    </div>
+    <h1>Create Playlist: </h1>
+    <h3>Name: </h3>
+     <input className="name-bar" type="text" placeholder="Name..."/>
+    <h3>Cover Picture: </h3>
+    <input className="name-bar" type="file"/>
+  </div>
+    <Button variant="light" className="creation-button">CREATE</Button>
+    <Button variant="danger" className="cancel-button">Cancel</Button>
+  </div>
   );
 };
 
