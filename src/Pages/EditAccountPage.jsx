8eeba6c73fdfc23c6e6f4f90f8b81cc00dde6989
Jose Ramos <jmrp.04@gmail.com>
2022-08-08 01:10:00 -0400

    Fixing bugs on the edit profile page

--- src/Pages/EditAccountPage.jsx
+++ src/Pages/EditAccountPage.jsx
@@ -12,7 +12,7 @@ import db from "../firebase";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import React, { useEffect, useState } from "react";
 import "./CSS/EditAccount.css";
-import { Button } from "react-bootstrap";
+import { Image, Button, Col, Row, Container, Grid } from "react-bootstrap";
 import { ToastAlert } from "../Components/Toast/index";
 
 // https://www.encodedna.com/javascript/redirect-page-after-a-delay-using-javascript.htm#:~:text=Try%20it%20You%20can%20call%20the%20redirect_Page%20%28%29,delay%20is%20for%205000%20milliseconds%20or%205%20seconds.
@@ -26,6 +26,7 @@ let redirect_Page = () => {
 const EditAccountPage = ({ user }) => {
   const [real_name, setName] = useState("");
   const [username, setUsername] = useState("");
+  const [currentUsername, setCurrentUsername] = useState("");
   const [bio, setBio] = useState("");
   const [image, setImage] = useState("");
   const [filePath, setFilePath] = useState();
@@ -39,6 +40,7 @@ const EditAccountPage = ({ user }) => {
     querySnapshot.forEach((document) => {
       setName(document.data().name);
       setUsername(document.data().username);
+      setCurrentUsername(document.data().username);
       setBio(document.data().bio);
       setPfp(document.data().pfp);
     });
@@ -81,8 +83,7 @@ const EditAccountPage = ({ user }) => {
       where("username", "==", username)
     );
     const snapshot = await getDocs(que);
-    if (snapshot.docs.length != 0) {
-      console.log("lol");
+    if (snapshot.docs.length != 0 && username != currentUsername) {
       ToastAlert("This username is already in use.", "error");
       return;
     }
@@ -138,13 +139,48 @@ const EditAccountPage = ({ user }) => {
             placeholder="Enter something about yourself..."
           ></textarea>
           <h3 className="ea-text">Profile Picture: </h3>
-          <input
-            className="file-bar"
-            type="file"
-            onChange={(e) => {
-              setImage(e.target.files[0]);
-            }}
-          />
+          {/* <Grid fluid style={{ paddingLeft: 0, paddingRight: 0 }}> */}
+          <Container className="g-0 p-0 m-0">
+            <Row md={6}>
+              <Col>
+                <Image
+                  width={200}
+                  height={200}
+                  className="col img-thumbnail"
+                  src={pfp}
+                  alt="pfp"
+                  onError={(event) => {
+                    event.target.src =
+                      "https://i1.wp.com/suiteplugins.com/wp-content/uploads/2019/10/blank-avatar.jpg?ssl=1";
+                    event.onerror = null;
+                  }}
+                />
+              </Col>
+              <Col>
+                <Container>
+                  <Col>
+                    <input
+                      className="file-bar"
+                      type="file"
+                      onChange={(e) => {
+                        setImage(e.target.files[0]);
+                      }}
+                    />
+                  </Col>
+                  <Col>
+                    <Button
+                      variant="danger"
+                      className="remove-pfp"
+                      onClick={editAccount}
+                    >
+                      Remove Picture
+                    </Button>
+                  </Col>
+                </Container>
+              </Col>
+            </Row>
+          </Container>
+          {/* </Grid> */}
         </div>
         <Button
           variant="light"
